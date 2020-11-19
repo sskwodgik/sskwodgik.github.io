4043d24ce9363a42924642bad747fac268a45e67
sskwodgik <ssk@wodgik.katowice.pl>
2020-11-19 20:00:30 +0100

    poprawa

--- pobierz-dane-click/js/script.js
+++ pobierz-dane-click/js/script.js
@@ -1,14 +1,20 @@
 let button = document.getElementById('pobierzDane');
 
 const getData = () => {
+    let id = document.getElementById('id');
+    let userId = document.getElementById('userId');
     let title = document.getElementById('title');
+    let body = document.getElementById('body');
     fetch('https://akademia108.pl/api/ajax/get-post.php', {
     mode: 'cors',    
     method: 'GET'
     })
     .then(res => res.json())
     .then((out) => {
-        title.innerHTML = "<p>asasa</p>";
+        id.innerHTML = "<p>ID: "+out.id+"</p>";
+        userId.innerHTML = "<p>UserID: "+out.userId+"</p>";
+        title.innerHTML = "<p>Title: "+out.title+"</p>";
+        body.innerHTML = "<p>Body: "+out.body+"</p>";
 
     })
     .catch(err => { throw err })
